66408278c5e8cbfbcd3fa56d75d307ccf03a11b2
Sergey2311 <sergey556577@gmail.com>
2019-12-26 22:44:21 +0300

    Рабочий вариант

---
+++ app.js
@@ -0,0 +1,147 @@
+import { Api } from './lib.js';
+
+
+const rootEl = document.getElementById('root')
+
+const addFormEl = document.createElement('form');
+
+addFormEl.innerHTML = `
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <input data-id="link" class="form-control" type="text" placeholder="Введите текст или вставьте ссылку">
+        </div>
+        <div class="col-4">
+            <select data-id="type" class="form-control">
+                <option value="regular">Обычный</option>
+                <option value="image">Картинка</option>
+                <option value="video">Видео</option>
+                <option value="audio">Аудио</option>
+            </select>
+        </div>
+        <div class="col">
+            <button data-id="button" class="btn btn-primary">Ок</button>
+        </div>
+    </div>
+</div>    
+`;
+rootEl.appendChild(addFormEl);
+const api = new Api('https://myserver7-8.herokuapp.com');
+
+const linkEl = addFormEl.querySelector('[data-id=link]');
+linkEl.value = localStorage.getItem('link');
+linkEl.addEventListener('input', (ev) => {
+    localStorage.setItem('link', ev.currentTarget.value)
+});
+const typeEl = addFormEl.querySelector('[data-id=type]');
+typeEl.value = localStorage.getItem('type');
+typeEl.addEventListener('input', (ev) => {
+    localStorage.setItem('type', ev.currentTarget.value)
+});
+
+const buttonEl = addFormEl.querySelector('[data-id=button]');
+buttonEl.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    const data = {
+        id: 0,
+        type: typeEl.value,
+        value: linkEl.value,
+    };
+    
+    api.postJSON('/posts',data, (ev) => {
+        loadData();
+        linkEl.value = '';
+        typeEl.value = 'Обычный';
+        localStorage.clear();
+        },handleError());
+});
+
+const postsEl = document.createElement('div');
+rootEl.appendChild(postsEl);
+
+const rebuildList = data => {
+    postsEl.innerHTML = '';
+
+    data.sort((a, b) => {
+        return b.likes - a.likes
+    });
+
+    for (const item of data) {
+        const postEl = document.createElement('div');
+        postEl.className = 'card mb-2'
+        if (item.type == 'image') {
+            postEl.innerHTML = `
+            <div class = "col px-md-7">
+                <img src = "${item.value}" class = "card-img-top" width = "100" height = "100">
+                <div class = "card-body">
+                    <button data-action = "like" class = "btn btn-primary">♡${item.likes}</button>
+                    <button data-action = "dislike" class = "btn btn-primary">👎🏻</button>
+                    <button data-action = "remove" class = "btn btn-primary">delete</button>
+                </div>
+            </div>
+            `;
+        }
+        else if (item.type == 'video') {
+            postEl.innerHTML = `
+            <div class = "col px-md-5">
+                <div class = "card-img-topcard-img-top embed-responsive embed-responsive-16by9 mb-2">
+                    <video src = "${item.value}" class = "embed-responsive-item" controls>
+                </div>
+                <div class = "col">
+                    <button data-action = "like" class = "btn btn-primary">♡ ${item.likes} </button>
+                    <button data-action = "dislike" class = "btn btn-primary">👎🏻</button>
+                    <button data-action = "remove" class = "btn btn-primary">delete</button>
+                </div>
+            </div>
+            `;
+        }
+
+        else if (item.type == 'audio') {
+            postEl.innerHTML = `
+            <div class = "col px-md-5">
+                <div class = "card-img-topcard-img-top embed-responsive embed-responsive-16by9 mb-2">
+                    <audio src = "${item.value}" class = "embed-responsive-item" controls>
+                </div>
+                <div class ="col">
+                    <button data-action = "like" class = "btn btn-primary">♡${item.likes}</button>
+                    <button data-action = "dislike" class = "btn btn-primary">👎🏻</button>
+                    <button data-action = "remove" class = "btn btn-primary">delete</button>
+                </div>
+            </div>
+            `;
+
+        }
+        else if (item.type == 'regular') {
+            postEl.innerHTML = `
+            <div class = "card">
+                <div class = "card-body">
+                    <p class="card-text">${item.value}</p>
+                    <button data-action = "like" class = "btn btn-primary">♡${item.likes}</button>
+                    <button data-action = "dislike" class = "btn btn-primary">👎🏻</button>
+                    <button data-action = "remove" class = "btn btn-primary">delete</button>
+                </div>
+            </div>
+            `;
+        }
+        postEl.addEventListener('click', (ev) => {
+            if (ev.target.dataset.action === 'like') {
+                api.postJSON(`/posts/${item.id}/likes`, rebuildList, handleError);
+            } else if (ev.target.dataset.action === 'dislike') {
+                api.deleteJSON(`/posts/${item.id}/dislikes`, rebuildList, handleError);
+            } else if (ev.target.dataset.action === 'remove') {
+                api.deleteJSON(`/posts/${item.id}`, rebuildList, handleError);
+            }
+        })
+        postsEl.appendChild(postEl);
+    }
+}
+
+
+
+const handleError = (ev) => {
+    console.log(ev);
+};
+const loadData = () => {
+    api.getJSON('/posts', rebuildList, handleError);
+};
+loadData();
---
+++ lib.js
@@ -0,0 +1,52 @@
+export class Api {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+    getJSON(path, onSuccess, onFail) {
+        this.makeRequest(path, 'GET', [], null, onSuccess, onFail)
+    };
+
+    postJSON(path, data, onSuccess, onFail) {
+        if (data === null) {
+            this.makeRequest(path, 'POST', [], null, onSuccess, onFail)
+            return;
+        }
+        this.makeRequest(path, 'POST', [
+            {name: 'Content-Type', value: 'application/json'}
+        ], JSON.stringify(data), onSuccess, onFail);
+    };
+
+    deleteJSON(path, onSuccess, onFail) {
+        this.makeRequest(path, 'DELETE', [], null, onSuccess, onFail)
+    };
+
+
+
+    makeRequest(path, method, headers = [], body=null, onSuccess, onError) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, `${this.baseUrl}${path}`);
+        for (const header of headers) {
+            xhr.setRequestHeader(header.name, header.value);
+        }
+        xhr.addEventListener('load', ev => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const data = JSON.parse(xhr.responseText);
+                if (onSuccess !== undefined) {
+                    onSuccess(data)
+                    return;
+                }
+                return;
+            }
+            if (onError !== undefined) {
+                onError(xhr.statusText)
+                return;
+            }
+        });
+        xhr.addEventListener('error', ev => {
+            if (onError !== undefined) {
+                onError('Unexpected error');
+                }
+        });
+        xhr.send(body);
+    };
+};
